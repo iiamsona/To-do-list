a67d52077856bffcddf411a441521a4683f2555a
sona <sona22hakobyan@gmail.com>
2023-07-04 18:37:53 +0400

    newText multiple changes

--- scripts/components/boxes/ChnageText.js
+++ scripts/components/boxes/ChnageText.js
@@ -1,18 +1,20 @@
 export default function changeText() {
   const maxLength = 30;
+
   const inputElement = document.createElement("input");
   inputElement.classList.add("inputElement");
   inputElement.value = this.textContent;
-  const newTitle = document.createElement("p");
-  newTitle.classList.add("newTitle");
+
   this.parentNode.replaceChild(inputElement, this);
+
   inputElement.addEventListener("keydown", (event) => {
     inputElement.addEventListener("input", function () {
       this.value = this.value.slice(0, maxLength);
     });
+
     if (event.keyCode === 13) {
-      newTitle.innerHTML = inputElement.value;
-      inputElement.parentNode.replaceChild(newTitle, inputElement);
+      this.innerHTML = inputElement.value;
+      inputElement.parentNode.replaceChild(this, inputElement);
     }
   });
 }
