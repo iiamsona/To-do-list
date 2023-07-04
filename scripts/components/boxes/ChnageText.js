3642c49b16fa8250555e0402329367ca207cc71a
sona <sona22hakobyan@gmail.com>
2023-07-04 17:51:41 +0400

    newText char length max

--- scripts/components/boxes/ChnageText.js
+++ scripts/components/boxes/ChnageText.js
@@ -1,4 +1,5 @@
 export default function changeText() {
+  const maxLength = 30;
   const inputElement = document.createElement("input");
   inputElement.classList.add("inputElement");
   inputElement.value = this.textContent;
@@ -6,6 +7,9 @@ export default function changeText() {
   newTitle.classList.add("newTitle");
   this.parentNode.replaceChild(inputElement, this);
   inputElement.addEventListener("keydown", (event) => {
+    inputElement.addEventListener("input", function () {
+      this.value = this.value.slice(0, maxLength);
+    });
     if (event.keyCode === 13) {
       newTitle.innerHTML = inputElement.value;
       inputElement.parentNode.replaceChild(newTitle, inputElement);
